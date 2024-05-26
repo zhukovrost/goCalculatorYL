d235fbaad08885153b7e630965fe48db8843cfd3
Ростислав Жуков <rosf.zhukov@gmail.com>
2024-05-26 19:08:50 +0300

    flags instead of constant values

--- cmd/orchestrator/main.go
+++ cmd/orchestrator/main.go
@@ -1,25 +1,30 @@
 package main
 
 import (
+	"flag"
 	"goCalculatorYL/internal/app"
 	"goCalculatorYL/internal/config"
 	"goCalculatorYL/internal/service"
 )
 
 func main() {
-	// настройки
 	const (
-		TIME_ADDITION_MS        = 1500
-		TIME_SUBTRACTION_MS     = 2000
-		TIME_MULTIPLICATIONS_MS = 5000
-		TIME_DIVISIONS_MS       = 4000
-		PORT                    = 8080
-		DEBUG_LEVEL             = true
+		PORT = 8080
 	)
 
-	logger := config.LoadLogger(DEBUG_LEVEL) // загрузка логгера
-	cfg, err := config.LoadConfig(PORT, TIME_ADDITION_MS, TIME_SUBTRACTION_MS,
-		TIME_MULTIPLICATIONS_MS, TIME_DIVISIONS_MS) // Загрузка конфигурации
+	// Определение флагов (подробнее go run cmd/orchestrator/main.go -h)
+	TIME_ADDITION_MS := flag.Uint("add", 5000, "time for addition operation in milliseconds")
+	TIME_SUBTRACTION_MS := flag.Uint("sub", 6000, "time for subtraction operation in milliseconds")
+	TIME_MULTIPLICATIONS_MS := flag.Uint("mul", 10000, "time for multiplication operation in milliseconds")
+	TIME_DIVISIONS_MS := flag.Uint("div", 11000, "time for division operation in milliseconds")
+	debug := flag.Bool("debug", false, "enable debug level logging")
+
+	// Парсинг флагов
+	flag.Parse()
+
+	logger := config.LoadLogger(*debug) // загрузка логгера
+	cfg, err := config.LoadConfig(PORT, *TIME_ADDITION_MS, *TIME_SUBTRACTION_MS,
+		*TIME_MULTIPLICATIONS_MS, *TIME_DIVISIONS_MS) // Загрузка конфигурации
 	if err != nil {
 		logger.Fatalf("Could not load config: %s\n", err.Error())
 		return
